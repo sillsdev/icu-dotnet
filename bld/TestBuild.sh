58e09350f6beb4589a34c4ef105a7f101a08456f
Daniel Glassey <daniel_glassey@sil.org>
2011-12-19 13:56:15 +0700

    add mono targets and mono specific build proj options to link against newer 4.x icu

--- bld/TestBuild.sh
+++ bld/TestBuild.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-xbuild /target:Build /property:teamcity_build_checkoutDir=/media/mono/icu-dotnet  /property:teamcity_dotnet_nunitlauncher_msbuild_task="notthere" /property:BUILD_NUMBER="4.2.1.abcd" /property:Minor="1"
+xbuild /target:Test /p:icu_ver=42 /p:Configuration=DebugMono /property:teamcity_build_checkoutDir=/home/daniel/Repos/Palaso/icu-dotnet  /property:teamcity_dotnet_nunitlauncher_msbuild_task="notthere" /property:BUILD_NUMBER="4.2.1.abcd" /property:Minor="1" build.mono.proj
