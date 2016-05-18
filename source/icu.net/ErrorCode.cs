032ed95f6ef31c194da27dc13377b9a3139e4d5b
Connie Yau <conniey@microsoft.com>
2016-05-18 10:37:46 -0700

    Fixing success check for ErrorCode

--- source/icu.net/ErrorCode.cs
+++ source/icu.net/ErrorCode.cs
@@ -196,6 +196,27 @@ namespace Icu
 		ERROR_LIMIT = IDNA_ERROR_LIMIT      /** This must always be the last value to indicate the limit for UErrorCode (last error code +1) */
 	}
 
+	internal static class ErrorCodeExtensions
+	{
+		/// <summary>
+		/// Determines whether the operation was successful or not.
+		/// http://icu-project.org/apiref/icu4c/utypes_8h_source.html#l00709
+		/// </summary>
+		public static bool IsSuccess(this ErrorCode errorCode)
+		{
+			return errorCode <= ErrorCode.ZERO_ERROR;
+		}
+
+		/// <summary>
+		/// Determines whether the operation resulted in an error.
+		/// http://icu-project.org/apiref/icu4c/utypes_8h_source.html#l00714
+		/// </summary>
+		public static bool IsFailure(this ErrorCode errorCode)
+		{
+			return errorCode > ErrorCode.ZERO_ERROR;
+		}
+	}
+
 	internal class ExceptionFromErrorCode
 	{
 		public static void ThrowIfError(ErrorCode e)
