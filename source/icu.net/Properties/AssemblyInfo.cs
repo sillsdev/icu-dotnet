a3e230ecf58a896825baf1bc365ade4780444376
Eberhard Beilharz <eb1@sil.org>
2016-12-06 14:19:21 +0100

    Introduce GitVersion
    Use GitVersion to set assembly version numbers.

--- source/icu.net/Properties/AssemblyInfo.cs
+++ source/icu.net/Properties/AssemblyInfo.cs
@@ -22,15 +22,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("3439151b-347b-4321-9f2f-d5fa28b46477")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
