c4db3943cec738f90b16d40c5ceed5f19b236538
Eberhard Beilharz <eb1@sil.org>
2016-12-08 14:36:37 +0100

    Remove debian packaging files
    We no longer create debian packages, and the packaging files haven't
    been updated in a long time, so it's time to do some cleanup :-)

--- build/MakeSource.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-xbuild /target:SourcePackage /p:icu_ver=42 /p:Configuration=DebugMono /property:teamcity_build_checkoutDir=`pwd`/.. /property:BUILD_NUMBER="0.1.abcd" /property:BuildCounter=1 build.mono.proj
