fde4236d80d9d340fc3668e1ecdc70367f08922b
Connie Yau <conniey@microsoft.com>
2017-01-13 09:14:21 -0800

    Replacing unsupported exceptions in .NETStandard

---
+++ source/icu.net/Exceptions/BreakException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Icu
+{
+	/// <summary>
+	/// Exceptions related to BreakIterator functionality.
+	/// </summary>
+	public class BreakException : Exception
+	{
+		/// <summary>
+		/// Creates exception with the provided message.
+		/// </summary>
+		/// <param name="message">The message that describes the error.</param>
+		public BreakException(string message) : base(message)
+		{ }
+	}
+}
---
+++ source/icu.net/Exceptions/IDNAException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Icu
+{
+	/// <summary>
+	/// Exception thrown when an IDNA ErrorCode is thrown.
+	/// </summary>
+	public class IDNAException : Exception
+	{
+		/// <summary>
+		/// Create an IDNA Exception with the following message.
+		/// </summary>
+		/// <param name="message">Message to pass to exception</param>
+		public IDNAException(string message) : base(message)
+		{ }
+	}
+}
