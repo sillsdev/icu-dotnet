f7d6f96b4d64416c0989c0ec323312efe85233a4
Connie Yau <conniey@microsoft.com>
2017-01-13 09:14:21 -0800

    Replacing unsupported exceptions in .NETStandard

---
+++ source/icu.net/Exceptions/MissingResourceException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Icu
+{
+	/// <summary>
+	/// Exception when icu4c cannot find a resource.
+	/// </summary>
+	public class MissingResourceException : Exception
+	{
+		/// <summary>
+		/// Creates exception with the provided message.
+		/// </summary>
+		/// <param name="message">The message that describes the error.</param>
+		public MissingResourceException(string message) : base(message)
+		{ }
+	}
+}
---
+++ source/icu.net/Exceptions/TransliteratorParseException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Icu
+{
+	/// <summary>
+	/// Exceptions for Transliterator errors.
+	/// </summary>
+	public class TransliteratorParseException : Exception
+	{
+		/// <summary>
+		/// Creates exception with the provided message.
+		/// </summary>
+		/// <param name="message">The message that describes the error.</param>
+		public TransliteratorParseException(string message) : base(message)
+		{ }
+	}
+}
