6e9f88b8dc6d13b2744862f51be97a0c5f20046a
Connie Yau <conniey@microsoft.com>
2017-01-13 14:44:00 -0800

    Change TestCase.Result to TestCase.ExpectedResult

--- source/icu.net.tests/NormalizerTests.cs
+++ source/icu.net.tests/NormalizerTests.cs
@@ -10,20 +10,20 @@ namespace Icu.Tests
 	[TestFixture]
 	public class NormalizerTests
 	{
-		[TestCase("XA\u0308bc", Normalizer.UNormalizationMode.UNORM_NFC, Result = "X\u00C4bc")]
-		[TestCase("X\u00C4bc", Normalizer.UNormalizationMode.UNORM_NFD, Result = "XA\u0308bc")]
-		[TestCase("tést", Normalizer.UNormalizationMode.UNORM_NFD, Result = "te\u0301st")]
-		[TestCase("te\u0301st", Normalizer.UNormalizationMode.UNORM_NFC, Result = "tést")]
-		[TestCase("te\u0301st", Normalizer.UNormalizationMode.UNORM_NFD, Result = "te\u0301st")]
+		[TestCase("XA\u0308bc", Normalizer.UNormalizationMode.UNORM_NFC, ExpectedResult = "X\u00C4bc")]
+		[TestCase("X\u00C4bc", Normalizer.UNormalizationMode.UNORM_NFD, ExpectedResult = "XA\u0308bc")]
+		[TestCase("tést", Normalizer.UNormalizationMode.UNORM_NFD, ExpectedResult = "te\u0301st")]
+		[TestCase("te\u0301st", Normalizer.UNormalizationMode.UNORM_NFC, ExpectedResult = "tést")]
+		[TestCase("te\u0301st", Normalizer.UNormalizationMode.UNORM_NFD, ExpectedResult = "te\u0301st")]
 		public string Normalize(string src, Normalizer.UNormalizationMode mode)
 		{
 			return Normalizer.Normalize(src, mode);
 		}
 
-		[TestCase("X\u00C4bc", Normalizer.UNormalizationMode.UNORM_NFC, Result = true)]
-		[TestCase("XA\u0308bc", Normalizer.UNormalizationMode.UNORM_NFC, Result = false)]
-		[TestCase("X\u00C4bc", Normalizer.UNormalizationMode.UNORM_NFD, Result = false)]
-		[TestCase("XA\u0308bc", Normalizer.UNormalizationMode.UNORM_NFD, Result = true)]
+		[TestCase("X\u00C4bc", Normalizer.UNormalizationMode.UNORM_NFC, ExpectedResult = true)]
+		[TestCase("XA\u0308bc", Normalizer.UNormalizationMode.UNORM_NFC, ExpectedResult = false)]
+		[TestCase("X\u00C4bc", Normalizer.UNormalizationMode.UNORM_NFD, ExpectedResult = false)]
+		[TestCase("XA\u0308bc", Normalizer.UNormalizationMode.UNORM_NFD, ExpectedResult = true)]
 		public bool IsNormalized(string src, Normalizer.UNormalizationMode expectNormalizationMode)
 		{
 			return Normalizer.IsNormalized(src, expectNormalizationMode);
