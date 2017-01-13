b746af0e22a0027840c9c0146aea532b52f61b83
Connie Yau <conniey@microsoft.com>
2017-01-13 09:14:21 -0800

    Replacing unsupported exceptions in .NETStandard

---
+++ source/icu.net/Exceptions/SyntaxErrorException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Icu
+{
+	/// <summary>
+	/// Exception for syntax errors in a format pattern (ie. Number, exponent patterns.)
+	/// </summary>
+	public class SyntaxErrorException : Exception
+	{
+		/// <summary>
+		/// Creates exception with the provided message.
+		/// </summary>
+		/// <param name="message">The message that describes the error.</param>
+		public SyntaxErrorException(string message) : base(message)
+		{ }
+	}
+}
