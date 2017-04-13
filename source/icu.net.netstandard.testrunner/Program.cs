c80f6451172447a60dcb2b5bf49ddc5337eb1945
Connie Yau <conniey@microsoft.com>
2017-04-13 13:18:51 -0700

    Adding NUnit test runner project due to nunit/dotnet-test-nunit#91

---
+++ source/icu.net.netstandard.testrunner/Program.cs
@@ -0,0 +1,15 @@
+﻿using Icu.Tests;
+using NUnitLite;
+using System.Reflection;
+
+namespace icu.net.netstandard.testrunner
+{
+	class Program
+	{
+		public static int Main(string[] args)
+		{
+			var assembly = typeof(BreakIteratorTests).GetTypeInfo().Assembly;
+			return new AutoRun(assembly).Execute(args);
+		}
+	}
+}
