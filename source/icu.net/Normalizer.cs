c48a679ab11c4e1672c7ac9f2bfd3e66c035dded
Eberhard Beilharz <eb1@sil.org>
2017-08-11 12:51:35 -0400

    Fix buffer overflow errors in Normalize()
    This change also starts to use unorm2 methods instead of the deprecated
    unorm* ones. The main change to fix the buffer overflow however is to
    reset the errorcode variable (Mono bug?).
    
    This fixes issue #47.

--- source/icu.net/Normalizer.cs
+++ source/icu.net/Normalizer.cs
@@ -31,6 +31,27 @@ namespace Icu
 			UNORM_FCD = 6
 		}
 
+		internal enum UNormalization2Mode
+		{
+			UNORM2_COMPOSE = 0,
+			UNORM2_DECOMPOSE = 1,
+			UNORM2_FCD = 2,
+			UNORM2_COMPOSE_CONTIGUOUS = 3
+		}
+
+		private static IntPtr GetNormalizer(UNormalizationMode mode)
+		{
+			ErrorCode errorCode;
+			var ret = NativeMethods.unorm2_getInstance(IntPtr.Zero,
+				(mode == UNormalizationMode.UNORM_NFC || mode == UNormalizationMode.UNORM_NFD) ? "nfc" : "nfkc",
+				(mode == UNormalizationMode.UNORM_NFC || mode == UNormalizationMode.UNORM_NFKC) ?
+					UNormalization2Mode.UNORM2_COMPOSE : UNormalization2Mode.UNORM2_DECOMPOSE,
+				out errorCode);
+			if (errorCode != ErrorCode.NoErrors)
+				throw new Exception("Normalizer.Normalize() failed with code " + errorCode);
+			return ret;
+		}
+
 		/// <summary>
 		/// Normalize the string according to the given mode.
 		/// </summary>
@@ -42,25 +63,26 @@ namespace Icu
 			if (string.IsNullOrEmpty(src))
 				return string.Empty;
 
-			int length = src.Length + 10;
-			IntPtr resPtr = Marshal.AllocCoTaskMem(length * 2);
+			var length = src.Length + 10;
+			var resPtr = Marshal.AllocCoTaskMem(length * 2);
 			try
 			{
 				ErrorCode err;
-				int outLength = NativeMethods.unorm_normalize(src, src.Length, mode, 0, resPtr, length, out err);
+				var normalizer = GetNormalizer(mode);
+				var outLength = NativeMethods.unorm2_normalize(normalizer, src, src.Length, resPtr, length, out err);
 				if (err > 0 && err != ErrorCode.BUFFER_OVERFLOW_ERROR)
 					throw new Exception("Normalizer.Normalize() failed with code " + err);
 				if (outLength >= length)
 				{
 					Marshal.FreeCoTaskMem(resPtr);
-					length = outLength + 1;		// allow room for the terminating NUL (FWR-505)
+					length = outLength + 1; // allow room for the terminating NUL (FWR-505)
 					resPtr = Marshal.AllocCoTaskMem(length * 2);
-					NativeMethods.unorm_normalize(src, src.Length, mode, 0, resPtr, length, out err);
+					outLength = NativeMethods.unorm2_normalize(normalizer, src, src.Length, resPtr, length, out err);
 				}
 				if (err > 0)
 					throw new Exception("Normalizer.Normalize() failed with code " + err);
 
-				string result = Marshal.PtrToStringUni(resPtr);
+				var result = Marshal.PtrToStringUni(resPtr);
 				// Strip any garbage left over at the end of the string.
 				if (err == ErrorCode.STRING_NOT_TERMINATED_WARNING && result != null)
 					return result.Substring(0, outLength);
@@ -86,7 +108,7 @@ namespace Icu
 				return true;
 
 			ErrorCode err;
-			byte fIsNorm = NativeMethods.unorm_isNormalized(src, src.Length, mode, out err);
+			var fIsNorm = NativeMethods.unorm2_isNormalized(GetNormalizer(mode), src, src.Length, out err);
 			if (err != ErrorCode.NoErrors)
 				throw new Exception("Normalizer.IsNormalized() failed with code " + err);
 			return fIsNorm != 0;
