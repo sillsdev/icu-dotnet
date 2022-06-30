92c487ae6b3f52e12efb23626fd63982b1ed18cd
Mark Kidder <mark_kidder@sil.org>
2022-06-30 11:52:49 -0400

    LT-21010: ToCharacters() fix bug with upper Unicode planes
    We were casting the Unicode code point into a char. Now we
    use char.ConvertFromUtf32().
    https://jira.sil.org/browse/LT-21010

--- source/icu.net/UnicodeSet.cs
+++ source/icu.net/UnicodeSet.cs
@@ -82,7 +82,7 @@ namespace Icu
 						// Add a character range to the set
 						for (var j = startChar; j <= endChar; j++)
 						{
-								output.Add(char.ConvertFromUtf32(j));
+							output.Add(char.ConvertFromUtf32(j));
 						}
 					}
 					else
