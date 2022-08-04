46eed8568715bc201f3918be06a32c9d39927339
Eberhard Beilharz <eb1@sil.org>
2022-08-04 12:32:15 +0200

    Fix tests with net461
    When running `dotnet test` on 64-bit Windows, the tests get run as 32-bit,
    but TestHelper will still be called as 64-bit app. This change checks
    the bitness of the OS instead of the running process.

--- source/icu.net/NativeMethods/NativeMethods.cs
+++ source/icu.net/NativeMethods/NativeMethods.cs
@@ -1,4 +1,4 @@
-// Copyright (c) 2013-2017 SIL International
+// Copyright (c) 2013-2022 SIL International
 // This software is licensed under the MIT license (http://opensource.org/licenses/MIT)
 
 using System;
