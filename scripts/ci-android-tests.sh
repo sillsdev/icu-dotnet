c297631272528971fbab63b8ccbe412fa469a209
Kevin Hahn <kevin_hahn@sil.org>
2026-07-03 13:43:53 +0700

    Run only Android smoke tests in CI until suite triage is done.

--- scripts/ci-android-tests.sh
+++ scripts/ci-android-tests.sh
@@ -17,4 +17,5 @@ dotnet restore "$PROJECT" -p:IcuDotNetIncludeAndroid=true
 dotnet test "$PROJECT" -f net10.0-android -c Release \
   -p:IcuDotNetIncludeAndroid=true \
   --no-restore \
+  --filter "FullyQualifiedName~icu.net.android.tests.Tests" \
   --logger "trx;LogFileName=test-results.trx"
